1eef62b2bab7e832678a64ce07d444dec9fe142d
JasonCLEI <765603496@qq.com>
2024-01-12 17:13:38 +0800

    updated postprocess_vcf.py

--- src/postprocess_vcf.py
+++ src/postprocess_vcf.py
@@ -186,16 +186,6 @@ def merge_vcf(args):
     if compress_vcf:
         compress_index_vcf(args.output_fn)
 
-    if args.enable_indel_calling and not args.indel_calling:
-        output_fn = args.output_fn + '.gz' if compress_vcf else args.output_fn
-        output_dir = os.path.dirname(output_fn)
-        file_name = output_fn.split('/')[-1]
-        if compress_vcf:
-            subprocess.run("cd {} && ln -sf {} snv.vcf.gz".format(output_dir, file_name), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            subprocess.run("cd {} && ln -sf {}.tbi snv.vcf.gz.tbi".format(output_dir, file_name), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        else:
-            subprocess.run("cd {} && ln -sf {} snv.vcf".format(output_dir, file_name), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
 def main():
     parser = ArgumentParser(description="VCF post-processing")
 
@@ -224,9 +214,6 @@ def main():
     parser.add_argument('--qual', type=float, default=None,
                         help="EXPERIMENTAL: If set, variants Phread quality with >=$qual will be marked 'PASS', or 'LowQual' otherwise")
 
-    parser.add_argument('--enable_indel_calling', type=str2bool, default=False,
-                        help=SUPPRESS)
-
     parser.add_argument('--af', type=float, default=None,
                         help=SUPPRESS)
 
@@ -234,9 +221,6 @@ def main():
                         help=SUPPRESS)
 
     parser.add_argument('--bed_format', action='store_true',
-                        help=SUPPRESS)
-
-    parser.add_argument('--indel_calling', action='store_true',
                         help=SUPPRESS)
 
     parser.add_argument('--prefer_recall', type=str2bool, default=False,
