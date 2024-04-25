043bbe6a18f48cb1e32f1a9bfc1b03d45531bb69
JasonCLEI <765603496@qq.com>
2024-04-25 19:45:25 +0800

    updated nonsomatic_tagging.py

--- src/nonsomatic_tagging.py
+++ src/nonsomatic_tagging.py
@@ -61,15 +61,12 @@ class VcfReader_Database(object):
                     self.header += row
                 continue
             chromosome, position = columns[0], columns[1]
-            id = str(columns[2])
             if self.ctg_name is not None and chromosome != self.ctg_name:
                 continue
             reference, alternate = columns[3], columns[4]
-            if (len(reference) > 1 or len(alternate) > 1):
-                continue
             position = int(position)
             row_str = row if self.keep_row_str else False
-            key = (chromosome, position, id) if self.ctg_name is None else (position, id)
+            key = (chromosome, position, reference, alternate) if self.ctg_name is None else (position, reference, alternate)
 
             self.variant_dict[key] = Position(ctg_name=chromosome,
                                               pos=position,
@@ -86,6 +83,8 @@ def nonsomatic_tag(args):
     pileup_vcf_fn = args.pileup_vcf_fn
     panel_of_normals = args.panel_of_normals
     pon_list = list(panel_of_normals.split(',')) if panel_of_normals is not None else []
+    panel_of_normals_require_allele_matching = args.panel_of_normals_require_allele_matching
+    pon_list_require_allele_matching = list(panel_of_normals_require_allele_matching.split(',')) if panel_of_normals_require_allele_matching is not None else []
 
     if panel_of_normals is not None:
         if len(pon_list) >= 1:
@@ -94,6 +93,14 @@ def nonsomatic_tag(args):
             pon_vcf_fn_list = []
     else:
         pon_vcf_fn_list = []
+
+    if panel_of_normals_require_allele_matching is not None:
+        if len(pon_list_require_allele_matching) >= 1:
+            pon_require_allele_matching_list = pon_list_require_allele_matching
+        else:
+            pon_require_allele_matching_list = []
+    else:
+        pon_require_allele_matching_list = []
 
     pileup_output_vcf_fn = args.output_vcf_fn
 
@@ -106,25 +113,26 @@ def nonsomatic_tag(args):
     input_vcf_reader.read_vcf()
     pileup_variant_dict = input_vcf_reader.variant_dict
 
+    input_variant_dict_set_contig = defaultdict(set)
     input_variant_dict_id_set_contig = defaultdict(set)
 
     for k, v in pileup_variant_dict.items():
         if ctg_name is None:
             if args.show_ref:
-                input_variant_dict_id_set_contig[k[0]].add(
-                    (str(k[1]) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0]))
+                input_variant_dict_set_contig[k[0]].add(str(k[1]))
+                input_variant_dict_id_set_contig[k[0]].add(str(k[1]) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0])
             else:
                 if v.filter == "PASS":
-                    input_variant_dict_id_set_contig[k[0]].add(
-                        (str(k[1]) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0]))
+                    input_variant_dict_set_contig[k[0]].add(str(k[1]))
+                    input_variant_dict_id_set_contig[k[0]].add(str(k[1]) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0])
         else:
             if args.show_ref:
-                input_variant_dict_id_set_contig[ctg_name].add(
-                    (str(k) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0]))
+                input_variant_dict_set_contig[ctg_name].add(str(k))
+                input_variant_dict_id_set_contig[ctg_name].add(str(k) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0])
             else:
                 if v.filter == "PASS":
-                    input_variant_dict_id_set_contig[ctg_name].add(
-                        (str(k) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0]))
+                    input_variant_dict_set_contig[ctg_name].add(str(k))
+                    input_variant_dict_id_set_contig[ctg_name].add(str(k) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0])
 
     nonsomatic_filter_variant_dict_id_set_contig = input_variant_dict_id_set_contig.copy()
 
@@ -142,17 +150,10 @@ def nonsomatic_tag(args):
     if len(pon_vcf_fn_list) > 0:
         for index, pon_vcf_fn in enumerate(pon_vcf_fn_list):
             input_inter_pon_variant_dict_id_set_contig = defaultdict(set)
-            if str(pon_vcf_fn).split('/')[-1] == 'dbsnp.b138.non-somatic.sites.vcf.gz':
-                pon_vcf_reader = VcfReader_Database(vcf_fn=str(pon_vcf_fn),
-                                                      ctg_name=ctg_name,
-                                                      keep_row_str=False,
-                                                      save_header=True)
-            else:
-                pon_vcf_reader = VcfReader(vcf_fn=str(pon_vcf_fn),
-                                                ctg_name=ctg_name,
-                                                keep_row_str=False,
-                                                filter_tag=None,
-                                                save_header=True)
+            pon_vcf_reader = VcfReader_Database(vcf_fn=str(pon_vcf_fn),
+                                                  ctg_name=ctg_name,
+                                                  keep_row_str=False,
+                                                  save_header=True)
             pon_vcf_reader.read_vcf()
             pon_input_variant_dict = pon_vcf_reader.variant_dict
 
@@ -162,21 +163,28 @@ def nonsomatic_tag(args):
                 if ctg_name is None:
                     if k[0] not in input_keys_list:
                         continue
-                    pon_variant_dict_id_set_contig[k[0]].add(
-                        (str(k[1]) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0]))
-                else:
-                    if str(pon_vcf_fn).split('/')[-1] == 'dbsnp.b138.non-somatic.sites.vcf.gz':
-                        pon_variant_dict_id_set_contig[ctg_name].add(
-                            (str(k[0]) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0]))
+                    if str2bool(pon_require_allele_matching_list[index]):
+                        for i in range(len(v.alternate_bases)):
+                            pon_variant_dict_id_set_contig[k[0]].add(str(k[1]) + '\t' + v.reference_bases + '\t' + v.alternate_bases[i])
                     else:
-                        pon_variant_dict_id_set_contig[ctg_name].add(
-                            (str(k) + '\t' + v.reference_bases + '\t' + v.alternate_bases[0]))
+                        pon_variant_dict_id_set_contig[k[0]].add(str(k[1]))
+                else:
+                    if str2bool(pon_require_allele_matching_list[index]):
+                        for i in range(len(v.alternate_bases)):
+                            pon_variant_dict_id_set_contig[ctg_name].add(str(k[0]) + '\t' + v.reference_bases + '\t' + v.alternate_bases[i])
+                    else:
+                        pon_variant_dict_id_set_contig[ctg_name].add(str(k[0]))
 
             for k, v in nonsomatic_filter_variant_dict_id_set_contig.items():
-                nonsomatic_filter_variant_dict_id_set_contig[k] = nonsomatic_filter_variant_dict_id_set_contig[k] - \
-                                                                  pon_variant_dict_id_set_contig[k]
-                input_inter_pon_variant_dict_id_set_contig[k] = input_variant_dict_id_set_contig[k].intersection(
-                    pon_variant_dict_id_set_contig[k])
+                if str2bool(pon_require_allele_matching_list[index]):
+                    nonsomatic_filter_variant_dict_id_set_contig[k] = nonsomatic_filter_variant_dict_id_set_contig[k] - \
+                                                                      pon_variant_dict_id_set_contig[k]
+                    input_inter_pon_variant_dict_id_set_contig[k] = input_variant_dict_id_set_contig[k].intersection(
+                        pon_variant_dict_id_set_contig[k])
+                else:
+                    nonsomatic_filter_variant_dict_id_set_contig[k] = {id for id in nonsomatic_filter_variant_dict_id_set_contig[k] if id.split('\t')[0] not in pon_variant_dict_id_set_contig[k]}
+                    input_inter_pon_variant_dict_id_set_contig[k] = input_variant_dict_set_contig[k].intersection(
+                        pon_variant_dict_id_set_contig[k])
 
             input_inter_pon_variant_dict_id_set_contig_list.append(
                 input_inter_pon_variant_dict_id_set_contig)
@@ -189,7 +197,7 @@ def nonsomatic_tag(args):
             print("[INFO] Processing in {}: tagged by {} PoN: {}".format(ctg_name, str(pon_vcf_fn), total_filter_by_pon))
 
             pon_vcf_md5 = calculate_file_md5(str(pon_vcf_fn))
-            pon_vcf_header_info = '##INFO=<ID=PoN_{},Number=0,Type=Flag,Description="file={},md5={},non-somatic variant tagged by panel of normals">'.format(index + 1, str(pon_vcf_fn), pon_vcf_md5)
+            pon_vcf_header_info = '##INFO=<ID=PoN_{},Number=0,Type=Flag,Description="file={},md5={},allele_matching={},non-somatic variant tagged by panel of normals">'.format(index + 1, str(pon_vcf_fn), pon_vcf_md5, pon_require_allele_matching_list[index])
             pon_vcf_header_info_list.append(pon_vcf_header_info)
 
             nonsomatic_tag_vcf_header_info += (pon_vcf_header_info + '\n')
@@ -237,10 +245,17 @@ def nonsomatic_tag(args):
                         info_str_db = ""
                         if len(input_inter_pon_variant_dict_id_set_contig_list) > 0:
                             for index, input_inter_pon_variant_dict_id_set_contig in enumerate(input_inter_pon_variant_dict_id_set_contig_list):
-                                if pos_info in input_inter_pon_variant_dict_id_set_contig[contig]:
-                                    if info_str_db != "":
-                                        info_str_db += ";"
-                                    info_str_db += "PoN_{}".format(str(index + 1))
+                                if str2bool(pon_require_allele_matching_list[index]):
+                                    if pos_info in input_inter_pon_variant_dict_id_set_contig[contig]:
+                                        if info_str_db != "":
+                                            info_str_db += ";"
+                                        info_str_db += "PoN_{}".format(str(index + 1))
+                                else:
+                                    if pos_info.split('\t')[0] in input_inter_pon_variant_dict_id_set_contig[contig]:
+                                        if info_str_db != "":
+                                            info_str_db += ";"
+                                        info_str_db += "PoN_{}".format(str(index + 1))
+
                         columns[7] = info_str_ori + ";" + info_str_db
                     row_str = '\t'.join(columns)
                     output.write(row_str)
@@ -262,6 +277,9 @@ def main():
 
     parser.add_argument('--panel_of_normals', type=str, default=None,
                         help="The path of the panel of normals (PoNs) used for tagging non-somatic variants. Split by ',' for multiple PoNs. Default: if not specified, provided 'gnomad.r2.1.af-ge-0.001.sites.vcf.gz', 'dbsnp.b138.non-somatic.sites.vcf.gz', and '1000g-pon.sites.vcf.gz' PoNs will be included")
+
+    parser.add_argument('--panel_of_normals_require_allele_matching', type=str, default=None,
+                        help="Whether to require allele matching when using corresponding PoNs. Split by ',' for multiple PoNs. Default: if not specified, using 'True', 'True', and 'False' for 'gnomad.r2.1.af-ge-0.001.sites.vcf.gz', 'dbsnp.b138.non-somatic.sites.vcf.gz', and '1000g-pon.sites.vcf.gz' PoNs respectively")
 
     parser.add_argument('--output_vcf_fn', type=str, default=None,
                         help="Output VCF file")
