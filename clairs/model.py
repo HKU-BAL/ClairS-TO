de290f138efb3c50eb932e93bcd400172f558fc8
JasonCLEI <765603496@qq.com>
2024-04-25 19:45:37 +0800

    updated model.py

--- clairs/model.py
+++ clairs/model.py
@@ -146,6 +146,7 @@ class Transformer(nn.Module):
             x = ff(x) + x
         return x
 
+
 class CvT(nn.Module):
     def __init__(
         self,
@@ -259,82 +260,85 @@ class CvT(nn.Module):
 
             return a_output, c_output, g_output, t_output
 
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_channels, out_channels, stride=1):
-        super().__init__()
-
-        # residual function
-        self.residual_function = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels * BasicBlock.expansion)
-        )
-
-        # shortcut
-        self.shortcut = nn.Sequential()
-
-        if stride != 1 or in_channels != BasicBlock.expansion * out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels * BasicBlock.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels * BasicBlock.expansion)
-            )
-
-    def forward(self, x):
-        return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
-
-
-class BiGRU(nn.Module):
+class CvT_Indel(nn.Module):
     def __init__(
-            self,
-            num_classes=2,
-            width=param.no_of_positions,
-            batch_first=True,
-            apply_softmax=False,
-            channel_size=param.pileup_channel_size,
-            model_type="acgt"
+        self,
+        num_classes=2,
+        s1_emb_dim=32,
+        s1_emb_kernel=3,
+        s1_emb_stride=2,
+        s1_proj_kernel=3,
+        s1_kv_proj_stride=2,
+        s1_heads=1,
+        s1_depth=1,
+        s1_mlp_mult=4,
+        s2_emb_dim=64,
+        s2_emb_kernel=3,
+        s2_emb_stride=2,
+        s2_proj_kernel=3,
+        s2_kv_proj_stride=2,
+        s2_heads=3,
+        s2_depth=2,
+        s2_mlp_mult=4,
+        s3_emb_dim=128,
+        s3_emb_kernel=3,
+        s3_emb_stride=2,
+        s3_proj_kernel=3,
+        s3_kv_proj_stride=2,
+        s3_heads=6,
+        s3_depth=10,
+        s3_mlp_mult=4,
+        dropout=0.,
+        dropout_fc=0.3,
+        depth=1,
+        width=param.no_of_positions,
+        dim=param.pileup_channel_size,
+        apply_softmax=False,
+        model_type="acgt"
     ):
         super().__init__()
         kwargs = dict(locals())
-
+        dim = dim
+        layers = []
         self.model_type = model_type
+        self.layers_prefix = ('s1', 's2', 's3')
+        for prefix in self.layers_prefix:
+            config, kwargs = group_by_key_prefix_and_remove_prefix(f'{prefix}_', kwargs)
+            layers.append(nn.Sequential(
+                nn.Conv2d(dim, config['emb_dim'], kernel_size = config['emb_kernel'], padding = (config['emb_kernel'] // 2), stride = config['emb_stride']),
+                LayerNorm(config['emb_dim']),
+                Transformer(dim = config['emb_dim'], proj_kernel = config['proj_kernel'], kv_proj_stride = config['kv_proj_stride'], depth = config['depth'], heads = config['heads'], mlp_mult = config['mlp_mult'], dropout = dropout)
+            ))
 
-        self.num_layers = 2
+            dim = config['emb_dim']
+
+        self.layer1 = layers[0]
+        self.layer2 = layers[1]
+        if len(self.layers_prefix) == 3:
+            self.layer3 = layers[2]
+
+        self.dropout_fc1 = nn.Dropout(p=dropout_fc)
+        self.dropout_fc2 = nn.Dropout(p=dropout_fc)
         self.flatten = nn.Flatten()
-        self.lstm_hidden_size = 128
-        self.lstm_hidden_size2 = 192
-        fc1_layer_size = 128
-        dropout_rate = 0.3
 
-        self.input_shape = [param.no_of_positions, self.lstm_hidden_size2 * 2]
+        depth_scale_size = cal_scale(input_size=depth, layers=len(self.layers_prefix))
+        width_scale_size = cal_scale(input_size=width, layers=len(self.layers_prefix))
+        fc1_shape = dim * depth_scale_size * width_scale_size
+        self.fc1 = nn.Linear(in_features=fc1_shape, out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=num_classes)
+        self.a_fc2 = nn.Linear(in_features=128, out_features=128)
+        self.c_fc2 = nn.Linear(in_features=128, out_features=128)
+        self.g_fc2 = nn.Linear(in_features=128, out_features=128)
+        self.t_fc2 = nn.Linear(in_features=128, out_features=128)
+        self.i_fc2 = nn.Linear(in_features=128, out_features=128)
+        self.d_fc2 = nn.Linear(in_features=128, out_features=128)
 
-        self.dim = channel_size
-        self.lstm = nn.GRU(input_size=self.dim, hidden_size=self.lstm_hidden_size, batch_first=batch_first,
-                           num_layers=1, bidirectional=True)
-
-        self.lstm_2 = nn.GRU(input_size=self.lstm_hidden_size * 2, hidden_size=self.lstm_hidden_size2,
-                             batch_first=batch_first,
-                             num_layers=1, bidirectional=True)
-
-        self.dropout_fc1 = nn.Dropout(p=dropout_rate)
-        self.fc1 = nn.Linear(in_features=self.input_shape[0] * self.input_shape[1], out_features=fc1_layer_size)
-        self.dropout_fc2 = nn.Dropout(p=dropout_rate)
-
-        self.fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
-
-        self.a_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-        self.c_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-        self.g_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-        self.t_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-
-        self.na_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-        self.nc_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-        self.ng_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-        self.nt_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
+        self.a_fc3 = nn.Linear(in_features=128, out_features=num_classes)
+        self.c_fc3 = nn.Linear(in_features=128, out_features=num_classes)
+        self.g_fc3 = nn.Linear(in_features=128, out_features=num_classes)
+        self.t_fc3 = nn.Linear(in_features=128, out_features=num_classes)
+        self.i_fc3 = nn.Linear(in_features=128, out_features=num_classes)
+        self.d_fc3 = nn.Linear(in_features=128, out_features=num_classes)
 
         self.selu = nn.SELU()
         self.apply_softmax = apply_softmax
@@ -342,99 +346,12 @@ class BiGRU(nn.Module):
             self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
-
-        output, hidden = self.lstm(x)
-        output, hidden = self.lstm_2(output)
-
-        output = self.flatten(output)
-        output = output.view(output.size(0), -1)
-        output = self.dropout_fc1(output)
-        output = self.selu(self.dropout_fc1(self.fc1(output)))
-
-        output = self.selu(self.dropout_fc2(self.fc2(output)))
-
-        if self.model_type == "acgt":
-
-            a_output = self.selu(self.a_fc3(output))
-            c_output = self.selu(self.c_fc3(output))
-            g_output = self.selu(self.g_fc3(output))
-            t_output = self.selu(self.t_fc3(output))
-
-            return a_output, c_output, g_output, t_output
-
-        elif self.model_type == "nacgt":
-
-            na_output = self.selu(self.na_fc3(output))
-            nc_output = self.selu(self.nc_fc3(output))
-            ng_output = self.selu(self.ng_fc3(output))
-            nt_output = self.selu(self.nt_fc3(output))
-
-            if self.apply_softmax:
-                na_output = self.softmax(na_output)
-                nc_output = self.softmax(nc_output)
-                ng_output = self.softmax(ng_output)
-                nt_output = self.softmax(nt_output)
-
-            return na_output, nc_output, ng_output, nt_output
-
-
-class BiGRU_ACGT(nn.Module):
-    def __init__(
-            self,
-            num_classes=2,
-            width=param.no_of_positions,
-            batch_first=True,
-            apply_softmax=False,
-            channel_size=param.pileup_channel_size,
-            model_type="acgt"
-    ):
-        super().__init__()
-        kwargs = dict(locals())
-
-        self.model_type = model_type
-
-        self.num_layers = 2
-        self.flatten = nn.Flatten()
-        self.lstm_hidden_size = 128
-        self.lstm_hidden_size2 = 192
-        fc1_layer_size = 128
-        dropout_rate = 0.3
-
-        self.input_shape = [param.no_of_positions, self.lstm_hidden_size2 * 2]
-
-        self.dim = channel_size
-        self.lstm = nn.GRU(input_size=self.dim, hidden_size=self.lstm_hidden_size, batch_first=batch_first,
-                           num_layers=1, bidirectional=True)
-
-        self.lstm_2 = nn.GRU(input_size=self.lstm_hidden_size * 2, hidden_size=self.lstm_hidden_size2,
-                             batch_first=batch_first,
-                             num_layers=1, bidirectional=True)
-
-        self.dropout_fc1 = nn.Dropout(p=dropout_rate)
-        self.fc1 = nn.Linear(in_features=self.input_shape[0] * self.input_shape[1], out_features=fc1_layer_size)
-        self.dropout_fc2 = nn.Dropout(p=dropout_rate)
-
-        self.fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
-
-        self.a_fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
-        self.c_fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
-        self.g_fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
-        self.t_fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
-
-        self.a_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-        self.c_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-        self.g_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-        self.t_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
-
-        self.selu = nn.SELU()
-        self.apply_softmax = apply_softmax
-        if self.apply_softmax:
-            self.softmax = nn.Softmax(dim=-1)
-
-    def forward(self, x):
-
-        output, hidden = self.lstm(x)
-        output, hidden = self.lstm_2(output)
+        x = torch.unsqueeze(x, -2)
+        x = x.permute(0, 3, 2, 1)
+        output = self.layer1(x)
+        output = self.layer2(output)
+        if len(self.layers_prefix) == 3:
+            output = self.layer3(output)
 
         output = self.flatten(output)
         output = output.view(output.size(0), -1)
@@ -446,19 +363,25 @@ class BiGRU_ACGT(nn.Module):
             c_output = self.selu(self.dropout_fc2(self.c_fc2(output)))
             g_output = self.selu(self.dropout_fc2(self.g_fc2(output)))
             t_output = self.selu(self.dropout_fc2(self.t_fc2(output)))
+            i_output = self.selu(self.dropout_fc2(self.i_fc2(output)))
+            d_output = self.selu(self.dropout_fc2(self.d_fc2(output)))
 
             a_output = self.selu(self.a_fc3(a_output))
             c_output = self.selu(self.c_fc3(c_output))
             g_output = self.selu(self.g_fc3(g_output))
             t_output = self.selu(self.t_fc3(t_output))
+            i_output = self.selu(self.i_fc3(i_output))
+            d_output = self.selu(self.d_fc3(d_output))
 
             if self.apply_softmax:
                 a_output = self.softmax(a_output)
                 c_output = self.softmax(c_output)
                 g_output = self.softmax(g_output)
                 t_output = self.softmax(t_output)
+                i_output = self.softmax(i_output)
+                d_output = self.softmax(d_output)
 
-            return a_output, c_output, g_output, t_output
+            return a_output, c_output, g_output, t_output, i_output, d_output
 
 
 class BiGRU_NACGT(nn.Module):
@@ -544,76 +467,94 @@ class BiGRU_NACGT(nn.Module):
             return na_output, nc_output, ng_output, nt_output
 
 
-class BasicConv2D(nn.Module):
-    def __init__(self, input_channel, output_channel, kernel_size=3, strides=1, padding=1):
+class BiGRU_NACGT_Indel(nn.Module):
+    def __init__(
+            self,
+            num_classes=2,
+            width=param.no_of_positions,
+            batch_first=True,
+            apply_softmax=False,
+            channel_size=param.pileup_channel_size,
+            model_type="acgt"
+    ):
         super().__init__()
+        kwargs = dict(locals())
 
-        self.conv = nn.Sequential(
-            nn.Conv2d(input_channel, output_channel, kernel_size=kernel_size, stride=strides, padding=padding,
-                      bias=False),
-            nn.BatchNorm2d(output_channel),
-            nn.ReLU(inplace=True))
+        self.model_type = model_type
 
-    def forward(self, x):
-        return self.conv(x)
-
-
-class ResNet(nn.Module):
-
-    def __init__(self, block=BasicBlock, num_block=1, platform='ont', num_classes=3):
-        super().__init__()
-
-        in_channels = param.channel_size
-        conv1_channel_size = 64
-        conv2_channel_size = 96
-        conv3_channel_size = 128
-        fc1_layer_size = 160
-        fc2_layer_size = 128
+        self.num_layers = 2
+        self.flatten = nn.Flatten()
+        self.lstm_hidden_size = 128
+        self.lstm_hidden_size2 = 192
+        fc1_layer_size = 128
         dropout_rate = 0.3
 
-        self.conv1 = BasicConv2D(input_channel=in_channels, output_channel=conv1_channel_size, strides=2)
+        self.input_shape = [param.no_of_positions, self.lstm_hidden_size2 * 2]
 
-        depth_scale_size = cal_scale(input_size=param.matrix_depth_dict[platform], layers=3)
-        width_scale_size = cal_scale(input_size=param.no_of_positions, layers=3)
+        self.dim = channel_size
+        self.lstm = nn.GRU(input_size=self.dim, hidden_size=self.lstm_hidden_size, batch_first=batch_first,
+                           num_layers=1, bidirectional=True)
 
-        self.conv1_x = self._make_layer(block, conv1_channel_size)
+        self.lstm_2 = nn.GRU(input_size=self.lstm_hidden_size * 2, hidden_size=self.lstm_hidden_size2,
+                             batch_first=batch_first,
+                             num_layers=1, bidirectional=True)
 
-        self.conv2 = BasicConv2D(input_channel=conv1_channel_size, output_channel=conv2_channel_size, strides=2)
-        self.conv2_x = self._make_layer(block, conv2_channel_size)
+        self.dropout_fc1 = nn.Dropout(p=dropout_rate)
+        self.fc1 = nn.Linear(in_features=self.input_shape[0] * self.input_shape[1], out_features=fc1_layer_size)
+        self.dropout_fc2 = nn.Dropout(p=dropout_rate)
 
-        self.conv3 = BasicConv2D(input_channel=conv2_channel_size, output_channel=conv3_channel_size, strides=2)
-        self.conv3_x = self._make_layer(block, conv3_channel_size)
+        self.fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
 
-        self.dropout = nn.Dropout(p=dropout_rate)
+        self.na_fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
+        self.nc_fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
+        self.ng_fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
+        self.nt_fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
+        self.ni_fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
+        self.nd_fc2 = nn.Linear(in_features=fc1_layer_size, out_features=fc1_layer_size)
+
+        self.na_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
+        self.nc_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
+        self.ng_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
+        self.nt_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
+        self.ni_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
+        self.nd_fc3 = nn.Linear(in_features=fc1_layer_size, out_features=num_classes)
+
         self.selu = nn.SELU()
-
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(conv3_channel_size * depth_scale_size * width_scale_size, fc1_layer_size)
-        self.fc2 = nn.Linear(fc1_layer_size, fc2_layer_size)
-        self.fc3 = nn.Linear(fc2_layer_size, num_classes)
-
-    def _make_layer(self, block, out_channels, stride=1):
-        strides = [stride]
-        layers = []
-        for stride in strides:
-            layers.append(block(out_channels, out_channels, stride))
-            self.in_channels = out_channels * block.expansion
-
-        return nn.Sequential(*layers)
+        self.apply_softmax = apply_softmax
+        if self.apply_softmax:
+            self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
-        output = self.conv1(x)
-        output = self.conv1_x(output)
-        output = self.conv2(output)
-        output = self.conv2_x(output)
-        output = self.conv3(output)
-        output = self.conv3_x(output)
+
+        output, hidden = self.lstm(x)
+        output, hidden = self.lstm_2(output)
 
         output = self.flatten(output)
         output = output.view(output.size(0), -1)
-        output = self.dropout(output)
-        output = self.selu(self.dropout(self.fc1(output)))
-        output = self.selu(self.dropout(self.fc2(output)))
-        output = self.selu(self.fc3(output))
+        output = self.dropout_fc1(output)
+        output = self.selu(self.dropout_fc1(self.fc1(output)))
 
-        return output
+        if self.model_type == "nacgt":
+            na_output = self.selu(self.dropout_fc2(self.na_fc2(output)))
+            nc_output = self.selu(self.dropout_fc2(self.nc_fc2(output)))
+            ng_output = self.selu(self.dropout_fc2(self.ng_fc2(output)))
+            nt_output = self.selu(self.dropout_fc2(self.nt_fc2(output)))
+            ni_output = self.selu(self.dropout_fc2(self.ni_fc2(output)))
+            nd_output = self.selu(self.dropout_fc2(self.nd_fc2(output)))
+
+            na_output = self.selu(self.na_fc3(na_output))
+            nc_output = self.selu(self.nc_fc3(nc_output))
+            ng_output = self.selu(self.ng_fc3(ng_output))
+            nt_output = self.selu(self.nt_fc3(nt_output))
+            ni_output = self.selu(self.ni_fc3(ni_output))
+            nd_output = self.selu(self.nd_fc3(nd_output))
+
+            if self.apply_softmax:
+                na_output = self.softmax(na_output)
+                nc_output = self.softmax(nc_output)
+                ng_output = self.softmax(ng_output)
+                nt_output = self.softmax(nt_output)
+                ni_output = self.softmax(ni_output)
+                nd_output = self.softmax(nd_output)
+
+            return na_output, nc_output, ng_output, nt_output, ni_output, nd_output
